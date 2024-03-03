28c59e8b4622f9495e9770c0a10bc6b5aa67aac3
jayyi <eyjian@qq.com>
2024-03-03 14:08:12 +0800

    Created

---
+++ example-02.sql
@@ -0,0 +1,14 @@
+DROP TABLE t_products;
+CREATE TABLE products ( -- 商品表
+                          id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '商品ID',
+                          name VARCHAR(255) NOT NULL UNIQUE COMMENT '商品名称',
+                          description TEXT COMMENT '商品描述',
+                          price DECIMAL(10, 2) NOT NULL COMMENT '商品价格',
+                          weight FLOAT NOT NULL COMMENT '商品重量（kg）',
+                          quantity SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '商品库存数量',
+                          is_active TINYINT(1) NOT NULL DEFAULT 1 COMMENT '商品是否激活（0 - 未激活，1 - 激活）',
+                          rating DOUBLE COMMENT '商品评分',
+                          created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '商品创建时间',
+                          updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '商品更新时间',
+                          INDEX idx_created_at (created_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
